2b11026d73d6c5d4d658407f690615946a19f829
Michael Theis (msg) <michael.theis@msg.group>
2023-05-02 18:34:38 +0200

    nexus/standalone: fixed some glitches in data volume mount

--- modules/nexus/standalone/ec2_user_data.tf
+++ modules/nexus/standalone/ec2_user_data.tf
@@ -22,13 +22,13 @@ mountDataVolume() {
   echo '*** Mounting Nexus data volume ***'
 
   echo "Wait for data volume to be attached"
-  while [[ "$(lsblk -f $DATA_BLOCK_DEVICE -o FSTYPE -n)" == *"no block device"* ]]
+  while [ "$(lsblk -f $DATA_BLOCK_DEVICE -o FSTYPE -n)" == *"not a block device"* ]
   do
     sleep 1
   done
 
   echo "Check if filesystem xfs is on data volume"
-  if [[ "$(lsblk -f $DATA_BLOCK_DEVICE -o FSTYPE -n)" == "xfs" ]]
+  if [ "$(lsblk -f $DATA_BLOCK_DEVICE -o FSTYPE -n)" == "xfs" ]
   then
     echo "filesystem xfs is already on data volume"
   else
