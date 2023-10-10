29aa931758c873b282d0ff6de7e69c5d4cc89f18
Michael Theis (msg) <michael.theis@msg.group>
2023-10-10 18:51:29 +0200

    module codebuild/project: made sure CodeBuild can push to private ECR repository

--- modules/codebuild/project/codebuild_role.tf
+++ modules/codebuild/project/codebuild_role.tf
@@ -101,7 +101,14 @@ resource "aws_iam_policy" "codebuild" {
     {
       "Effect": "Allow",
       "Action": [
-        "ecr:GetAuthorizationToken"
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchGetImage",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:CompleteLayerUpload",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:InitiateLayerUpload",
+        "ecr:PutImage",
+        "ecr:UploadLayerPart"
       ],
       "Resource": "*"
     }
